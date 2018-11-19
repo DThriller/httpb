1d4357a0823d51d0c18ca79b55e33fd4a0056e2c
Bảo Nguyễn Văn <nvbao45@gmail.com>
2018-11-19 22:36:53 +0700

    make it better

---
+++ index.js
@@ -0,0 +1,133 @@
+const PORT = 5000;
+
+var http = require('http');
+var express = require('express');
+var socketio = require('socket.io')
+var sqlite3 = require('sqlite3').verbose();
+var ip = require('ip');
+var app = express();
+
+var server = http.Server(app);
+var io = socketio(server);
+
+var webapp_nsp = io.of('/webapp');				//namespace của webapp
+var esp8266_nsp = io.of('/esp8266');			//namespace của esp8266
+
+var middleware = require('socketio-wildcard')();		//Để có thể bắt toàn bộ lệnh!
+esp8266_nsp.use(middleware);
+webapp_nsp.use(middleware);
+
+server.listen(process.env.PORT || PORT);
+console.log("Server: " + ip.address() + ":" + PORT)
+
+app.use(express.static("node_modules/mobile-angular-ui"))
+app.use(express.static("node_modules/angular"))
+app.use(express.static("node_modules/angular-route"))
+app.use(express.static("node_modules/socket.io-client"))
+app.use(express.static("node_modules/angular-socket-io"))
+app.use(express.static("webapp"))
+
+let db = new sqlite3.Database('./db/mydb.db', sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+	if (err) {
+		return console.log(err.message);
+	}
+	db.run(`CREATE TABLE DATALOG (Id INTEGER PRIMARY KEY AUTOINCREMENT, EventName TEXT, EventJson TEXT, Date TEXT)`, (err) => {
+		if (err){
+			console.log(err.message);
+		}
+	});
+});
+
+function ParseJson(jsondata) {
+	try {
+		return JSON.parse(jsondata);
+	} catch (error) {
+		return null;
+	}
+}
+
+function checkTableExist(tableName){
+	var sql = `SELECT * FROM sqlite_master WHERE type = 'table' AND name = '` + tableName + "'";
+	var existed = false;
+	db.all(sql, (err, row) => {
+		if (row.length == 1){
+			return true;
+		};
+	});
+};
+
+function insertData(packet){
+	var query = `insert into DATALOG(EventName, EventJson, Date) values('${packet.data[0]}','${JSON.stringify(packet.data[1])}', '${new Date()}')`;
+	//console.log(query);
+	db.run(query, function (err){
+		if (err){
+			console.log(err.message);
+		}
+	})
+}
+
+function getData(){
+	var query = `select * from DATALOG`;
+	db.all(query, function (err, row){
+		if (err){
+			console.log(err.message);
+		}
+		else {
+			row.forEach(element => {
+				console.log(element);
+			});
+		}
+	});
+};
+
+function closeDb(){
+	db.close((err) => {
+		if (err) {
+			console.error(err.message);
+		}
+		console.log('Close the database connection.');
+	});
+}
+
+esp8266_nsp.on('connection', function (socket) {
+	console.log('ESP8266: CONNECTED')
+
+	getData();
+
+	socket.on('disconnect', function () {
+		console.log("ESP8266: DISCONNECTED")
+	})
+
+	var date = new Date();
+
+	var dateData = {
+		time: [date.getHours(), date.getMinutes(), date.getSeconds()],
+		date: [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+	}
+	esp8266_nsp.emit("TIME", dateData);
+
+	socket.on("*", function (packet) {
+		console.log(`ESP8266: ${packet.data[0]} => ${JSON.stringify(packet.data[1])}`);
+		var eventName = packet.data[0]
+		var eventJson = packet.data[1] || {}
+
+		webapp_nsp.emit(eventName, eventJson)
+	})
+})
+
+
+webapp_nsp.on('connection', function (socket) {
+
+	console.log('WEBAPP: Connected');
+
+	socket.on('disconnect', function () {
+		console.log("WEBAPP: Disconnected")
+	})
+
+	socket.on('*', function (packet) {
+		console.log("WEBAPP: ", packet.data);
+		var eventName = packet.data[0];
+		var eventJson = packet.data[1] || {}
+		esp8266_nsp.emit(eventName, eventJson)
+	});
+})
---
+++ webapp/webapp.js
@@ -0,0 +1,96 @@
+﻿angular.module('myApp', ['ngRoute', 'mobile-angular-ui', 'btford.socket-io'])
+.config(function($routeProvider) {
+    $routeProvider.when('/', {
+        templateUrl: 'home.html',
+        controller: 'Home'
+    });
+})
+.factory('mySocket', function (socketFactory) {
+	var myIoSocket = io.connect('/webapp');
+
+	mySocket = socketFactory({
+		ioSocket: myIoSocket
+	});
+	return mySocket;
+
+})
+.controller('Home', function($scope, mySocket) {
+    $scope.temp = "";
+    $scope.tempF = "";
+	$scope.humidity = "";
+    $scope.hic = "";
+	$scope.leds_status = [0, 0, 0];
+	$scope.lamps = [0, 0, 0];
+	$scope.lampsLight = [ 50, 70, 80];
+	$scope.garage_status = 0;
+	$scope.lcd = ["", ""];
+
+	$scope.lcdPrint = function() {
+		var json = {
+			"line1": $scope.lcd[0],
+			"line2": $scope.lcd[1]
+		}
+		mySocket.emit("LCD", json);
+	}
+
+	$scope.lcdClear = function() {
+		mySocket.emit("CLEAR");
+	}
+
+	$scope.updateSensor  = function() {
+		mySocket.emit("DHT")
+	}
+	
+	$scope.updateDateTime = function(){
+		var date = new Date();
+		var json = {
+			time: [date.getHours(), date.getMinutes(), date.getSeconds()],
+			date: [date.getFullYear(), date.getMonth() + 1, date.getDate()]
+		}
+		mySocket.emit("TIME", json);
+	}
+	
+	$scope.changeLED = function() {
+		console.log("send LED ", $scope.leds_status)
+		
+		var json = {
+			"led": $scope.leds_status
+		}
+		mySocket.emit("LED", json)
+	}
+
+	$scope.openGarage = function() {
+		var json = {
+			'garage': $scope.garage_status
+		}
+		mySocket.emit("GARAGE", json)
+	}
+	
+	mySocket.on('DHT', function(json) {
+		$scope.temp = json.temC
+		$scope.tempF = json.temF
+		$scope.hic = json.hic
+		$scope.humidity = json.humidity
+	})
+
+	mySocket.on('LED_STATUS', function(json) {
+		console.log("recv LED", json)
+		$scope.leds_status = json.data
+	})
+	
+	mySocket.on('GARAGE', function(json) {
+		console.log('receive GARAGE status', json)
+		$scope.garage_status = json.data[0]
+	})
+	
+	mySocket.on('NOTE', function(json) {
+		console.log("NOTE", json);
+		$scope.lcd = json.data
+	})
+
+	mySocket.on('connect', function() {
+		console.log("connected")
+		mySocket.emit("LOADDATA")
+	})
+		
+});
